aed4dee9405a78459dab8f32c7bc81eaabe742c8
Chii <ohanasakuiroha@gmail.com>
2019-02-11 23:41:33 +0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const {watch, series} = require('gulp');
+const gulp = require('gulp');
+const server = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+//BROWSER
+// ====================================
+function serve(done) {
+	server.init({
+		server: {
+			baseDir: "./"
+		},
+		notify: false
+	});
+	done();
+};
+
+function reload(done) {
+	server.reload();
+	done();
+};
+
+// WATCH
+// =====================================
+function watchTask() {
+	watch("index.html", reload);
+
+	// sass
+	watch("./src/sass/**/*.scss", function() {
+		return gulp.src("./src/sass/**/*.scss")
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('./dist/css'))
+			.pipe(server.stream());
+	});
+};
+
+
+// EXPORTS
+exports.default = series(serve, watchTask);
